31fe06c08441ab5cd73a015dd629f267b9563333
Sile9t <eyesless1jack@gmail.com>
2024-06-07 21:59:17 +0300

    Added Client class init

--- Client.cs
+++ Client.cs
@@ -1,7 +1,20 @@
-﻿namespace EFDatabase
+﻿using EFDatabase.Abstracts;
+using EFDatabase.Services;
+
+namespace EFDatabase
 {
     public class Client
     {
-
+        string _name;
+        string _address;
+        int _port;
+        IMessageSource _messageSource;
+        public Client(string name, string address, int port)
+        {
+            _name = name;
+            _address = address;
+            _port = port;
+            _messageSource = new UDPMessageSource();
+        }
     }
 }
