36471898d7b94ed9db3f6c7bb282b5edfeb9226e
Sile9t <eyesless1jack@gmail.com>
2024-06-07 14:08:04 +0300

    Modified UDPServer.Listen method

--- Services/UDPServer.cs
+++ Services/UDPServer.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using EFDatabase.Abstracts;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -6,28 +7,42 @@ namespace EFDatabase.Services
 {
     public class UDPServer
     {
+        private readonly IMessageSource _messageSource;
+        public UDPServer()
+        {
+            _messageSource = new UDPMessageSource();
+        }
+        void ProcessMessage(NetMessage message)
+        {
+            switch (message.Command)
+            {
+                case Command.Register:
+                    break;
+                case Command.Message:
+                    break;
+                case Command.Confirmation:
+                    break;
+            }
+        }
         public async Task Listen()
         {
-            UdpClient udpClient = new UdpClient(12345);
+            //UdpClient udpClient = new UdpClient(12345);
             IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
             Console.WriteLine("Server is waiting for message");
 
             CancellationTokenSource cts = new CancellationTokenSource();
             while (!cts.IsCancellationRequested)
             {
-                //receiving msg
-                var buffer = udpClient.Receive(ref endPoint);
-                //decoding msg
-                var userMsg = Encoding.UTF8.GetString(buffer);
-                //encoding reply msg
-                var reply = Encoding.UTF8.GetBytes("Message received");
-                //callback to user
-                await udpClient.SendAsync(reply, endPoint);
-                //getting string from user msg
-                var message = NetMessage.DeserializeFromJson(userMsg);
-                //reply cancelling
-                if (message.Text.ToLower().Equals("exit")) cts.Cancel();
-                Console.WriteLine(message);
+                try
+                {
+                    var msg = _messageSource.Receive(endPoint);
+                    Console.WriteLine(msg);
+                    ProcessMessage(msg);
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine(ex);
+                }
             }
         }
     }
